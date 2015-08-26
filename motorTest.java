dda56a03dd4f6167b2d5667cfdf1104308bea8a0
Lucien Gaitskell <lucien@gaitskell.com>
2015-08-26 13:25:41 -0400

    added motorTest

---
+++ motorTest.java
@@ -0,0 +1,20 @@
+package org.wheeler.robotics.SensorTests;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by lucien on 8/26/15.
+ */
+public class motorTest extends OpMode {
+    DcMotor testMotor;
+    public void init() {
+        testMotor = hardwareMap.dcMotor.get("testMotor");
+    }
+    public void start() {
+        testMotor.setPower(0.1);
+    }
+    public void loop() {
+        telemetry.addData("1 Motor Speed: ", String.valueOf(testMotor.getPower()));
+    }
+}
